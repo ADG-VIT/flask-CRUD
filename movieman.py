8b9321f3296dbe92d84cd917b0f058585b704229
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-15 16:31:03 +0530

    barebones flask setup

---
+++ movieman.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "My flask app"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
