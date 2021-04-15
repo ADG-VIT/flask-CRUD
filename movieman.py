70408895fd74d5c6bae7f86dbd9dc89cd59d5992
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-15 16:36:37 +0530

    Creating Movie model

--- movieman.py
+++ movieman.py
@@ -1,6 +1,26 @@
+import os
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(
+    os.path.join(project_dir, "bookdatabase.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+
+
+class Movie(db.Model):
+    title = db.Column(db.String(80), unique=True,
+                      nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
 
 
 @app.route("/", methods=["GET", "POST"])
